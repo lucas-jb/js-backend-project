631c312e5345e2807fce9ddbd62d36b2306b2b89
lucas-jb <lucasjuarezbarrios@gmail.com>
2022-11-14 13:47:14 +0100

    update

--- html/server.js
+++ html/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-app.use('/assets', express.static('assets'));
+
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req,res)=>{
     res.sendFile('index.html',{
