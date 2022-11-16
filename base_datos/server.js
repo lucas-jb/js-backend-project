54eac4539cb2e317573d38c4cf476c3709864f26
lucas-jb <lucasjuarezbarrios@gmail.com>
2022-11-16 12:10:46 +0100

    update bbdd server

--- base_datos/server.js
+++ base_datos/server.js
@@ -1,5 +1,23 @@
+const express = require('express');
 const sqlite3 = require('sqlite3');
-let db = new sqlite3.Database('js-backend-project');
+const bodyParser = require('body-parser');
 
-db.run('CREATE TABLE tasks(id int AUTO_INCREMENT, description varchar(255))');
-db.close();
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+let db = new sqlite3.Database('js-backend-project');
+//db.run('CREATE TABLE tasks(id int AUTO_INCREMENT, description varchar(255))');
+
+app.post('/pendientes', (req,res)=>{
+    db.run("INSERT INTO tasks(description) VALUES('Hola mundo')");
+    res.send('Inserción finalizada')
+});
+
+app.listen(3300);
+
+process.on('SIGINT', ()=>{
+    console.log('Server closed.');
+    db.close();
+    process.exit();
+})
