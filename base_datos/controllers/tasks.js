4a71fb7ebcabe1b992716f7cd704a3acac5cbab1
lucas-jb <lucasjuarezbarrios@gmail.com>
2022-11-17 11:44:36 +0100

    update model, add pug views

---
+++ base_datos/controllers/tasks.js
@@ -0,0 +1,6 @@
+const Task = require('../models').Task;
+
+module.exports = {
+    home: (req,res)=>{}
+};
+
--- base_datos/server.js
+++ base_datos/server.js
@@ -3,19 +3,22 @@ const sqlite3 = require('sqlite3');
 const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
 
-
 const app = express();
+
 app.use(bodyParser.urlencoded({extended: true}));
 
-const sequelize = new Sequelize('js-backend-project', null, null, {
-    dialect: 'sqlite',
-    storage: './js-backend-project'
-});
+//const sequelize = new Sequelize('js-backend-project', null, null, {
+//    dialect: 'sqlite',
+//    storage: './js-backend-project'
+//});
+
+app.use('view engine', 'pug');
+
 let db = new sqlite3.Database('js-backend-project');
 
 app.post('/pendientes', (req,res)=>{
     //db.run(`INSERT INTO tasks(description) VALUES(?)`,req.body.description);
-    //res.send('Inserción finalizada')
+    res.send('Inserción finalizada')
 });
 
 app.listen(3300);
