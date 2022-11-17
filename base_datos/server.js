5cb4b6bf2bcdf112327725bf289c7e7f4077d6dd
lucas-jb <lucasjuarezbarrios@gmail.com>
2022-11-17 10:26:35 +0100

    add sequelize

--- base_datos/server.js
+++ base_datos/server.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
 const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+const sequelize = new Sequelize('js-backend-project', null, null, {
+    dialect: 'sqlite',
+    storage: './js-backend-project'
+});
 let db = new sqlite3.Database('js-backend-project');
 
 app.post('/pendientes', (req,res)=>{
-    db.run(`INSERT INTO tasks(description) VALUES(?)`,req.body.description);
-    res.send('Inserción finalizada')
+    //db.run(`INSERT INTO tasks(description) VALUES(?)`,req.body.description);
+    //res.send('Inserción finalizada')
 });
 
 app.listen(3300);
-
-process.on('SIGINT', ()=>{
-    console.log('Server closed.');
-    db.close();
-    process.exit();
-})
