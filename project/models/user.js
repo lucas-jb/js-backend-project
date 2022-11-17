dd7aea79174e89b7b4d6650d7813b8b0c06a1d3d
lucas-jb <lucasjuarezbarrios@gmail.com>
2022-11-17 18:35:21 +0100

    add bcrypt & update user model

--- project/models/user.js
+++ project/models/user.js
@@ -4,17 +4,16 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
   }
   User.init({
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
     password_hash: DataTypes.STRING
   }, {
     sequelize,
